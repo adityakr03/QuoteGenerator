647647736123de5686257c1b0aab6c2fbeded346
adityakr03 <leo03.akrs@gmail.com>
2024-03-16 00:47:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ function newQuote() {
     authorText.textContent = "~ " + author.trim(); // Trim any leading or trailing spaces
     quoteText.textContent = quote.text;
     complete();
+    generateGradientColor();
 }
 
 
@@ -58,3 +59,20 @@ twitterBtn.addEventListener('click', tweetQuote);
 
 // on load
 getQuotes();
+// Background Color change function
+function generateGradientColor() {
+    // Generate random RGB color values
+    let r1 = Math.floor(Math.random() * 256);
+    let g1 = Math.floor(Math.random() * 256);
+    let b1 = Math.floor(Math.random() * 256);
+    let r2 = Math.floor(Math.random() * 256);
+    let g2 = Math.floor(Math.random() * 256);
+    let b2 = Math.floor(Math.random() * 256);
+
+    // Construct CSS gradient string
+    let gradientColor = `linear-gradient(to right, rgb(${r1}, ${g1}, ${b1}), rgb(${r2}, ${g2}, ${b2}))`;
+
+    // Set body background to gradient color
+    document.body.style.background = gradientColor;
+}
+generateGradientColor();
